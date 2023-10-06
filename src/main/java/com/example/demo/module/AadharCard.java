8135bb0c7c718a7cc4b65d2d321e4b2512be0dc4
Shashanka <Shashanka@DESKTOP-QUDTRKQ>
2023-10-06 12:53:02 +0530

    second commit

--- src/main/java/com/example/demo/module/AadharCard.java
+++ src/main/java/com/example/demo/module/AadharCard.java
@@ -2,6 +2,9 @@ package com.example.demo.module;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class AadharCard {
@@ -9,10 +12,14 @@ public class AadharCard {
 
 
 		@Id
+		@Size(min = 12,max = 12,message="aadharNumber should be 12-digit")
+		@NotNull
 		private String adharNumber;
+		
+		@OneToOne(mappedBy = "aadharCard")
+		private Person person; 
 
-
-
+		
 		public String getAdharNumber() {
 			return adharNumber;
 		}
